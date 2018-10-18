a9f1d68264b1beb97fb9a7724c431804d265473f
Kamrin Langan <kl1060@cs.ship.edu>
2018-10-18 17:08:16 -0400

    added plasmacannon tests

--- src/main/java/weapon/PlasmaCannon.java
+++ src/main/java/weapon/PlasmaCannon.java
@@ -3,7 +3,7 @@ package weapon;
 import exceptions.WeaponException;
 
 public class PlasmaCannon extends GenericWeapon {
- 
+
   public PlasmaCannon() {
     maxAmmo = 4;
     currentAmmo = maxAmmo;
@@ -15,21 +15,21 @@ public class PlasmaCannon extends GenericWeapon {
   @Override
   public void updateTime(int time) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public int fire(int distance) throws WeaponException {
-    
+
     double damage = baseDamage * (Double.valueOf(currentAmmo) / Double.valueOf(maxAmmo));
     if (distance < 0) {
       throw new WeaponException("Distance must be greater than or equal to 0!");
     }
-    
+
     if (distance > maxRange) {
       damage = 0;
     }
-    if (currentAmmo > 0)  {
+    if (currentAmmo > 0) {
       currentAmmo--;
     }
     return Double.valueOf(Math.floor(damage)).intValue();
@@ -37,7 +37,7 @@ public class PlasmaCannon extends GenericWeapon {
 
   @Override
   public String toString() {
-    
+
     return "PlasmaCannon";
   }
 }
--- src/test/java/weapon/TestPlasmaCannon.java
+++ src/test/java/weapon/TestPlasmaCannon.java
@@ -4,11 +4,27 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import exceptions.WeaponException;
+
 public class TestPlasmaCannon {
 
+  /**
+   * Kamrin Langan
+   */
+
+  /**
+   * tests that plasma cannon does appropriate damage at certain distances
+   * 
+   * @throws WeaponException
+   */
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void testDamageAtADistance() throws WeaponException {
+
+    PlasmaCannon pc = new PlasmaCannon();
+
+    assertEquals(50, pc.fire(15));
+
+    assertEquals(0, pc.fire(55));
   }
 
 }
