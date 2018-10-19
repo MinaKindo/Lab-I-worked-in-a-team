4b6cea0cb3a0ae351115d9036438c89150a99a6e
Aminata Amadou Kindo <aa1184@ship.edu>
2018-10-19 13:58:36 -0400

    Class Pistol and Test Pistol

--- src/test/java/weapon/MockWeapon.java
+++ src/test/java/weapon/MockWeapon.java
@@ -1,8 +1,28 @@
 package weapon;
 
+import exceptions.WeaponException;
+
 public class MockWeapon extends GenericWeapon {
 
   public MockWeapon() {
     super();
+  }
+
+  @Override
+  public void updateTime(int time) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public int fire(int distance) throws WeaponException {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public String toString() {
+    // TODO Auto-generated method stub
+    return null;
   }
 }
