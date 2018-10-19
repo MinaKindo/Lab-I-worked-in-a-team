8a6672af3f50417b30bac50b3eadc2ba7686801b
Michael Mitchell <mm9466@ship.edu>
2018-10-19 11:51:13 -0400

    testing plasma cannon

--- src/test/java/weapon/TestPlasmaCannon.java
+++ src/test/java/weapon/TestPlasmaCannon.java
@@ -4,11 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import exceptions.WeaponException;
+
 public class TestPlasmaCannon {
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void testTwoDistances() throws WeaponException {
+    PlasmaCannon pc = new PlasmaCannon();
+    
+    assertEquals(50, pc.fire(15));
+    assertEquals(37, pc.fire(10));
   }
 
 }
